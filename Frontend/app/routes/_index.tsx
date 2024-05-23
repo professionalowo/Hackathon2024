a63923366b5f8656ee0bec8f07c913576effe0f3
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-23 19:10:57 +0200

    added some filler text

--- Frontend/app/routes/_index.tsx
+++ Frontend/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { NavLink } from "@remix-run/react";
 
 
 export const meta: MetaFunction = () => {
@@ -10,6 +11,13 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <p>Hello</p>
+    <div className="gap-5 px-5 py-3 flex flex-col w-full h-full justify-center align-middle items-center font-mono">
+      <h1 className="text-4xl">Welcome to Remix!</h1>
+      <p className="text-center text-lg">
+        This is a new Remix app. You can start editing this file immediately to
+        see how fast the hot module reloading is!
+      </p>
+      <NavLink className="px-5 py-3 bg-purple-800 hover:bg-purple-900 rounded-md" to={"/chat"}>Start Chatting</NavLink>
+    </div>
   );
 }
