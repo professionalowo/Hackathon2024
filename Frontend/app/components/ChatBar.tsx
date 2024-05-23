1aed571973eab3af2c7c16dc7bfb523075a163a0
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-24 01:09:42 +0200

    removed undefined

--- Frontend/app/components/ChatBar.tsx
+++ Frontend/app/components/ChatBar.tsx
@@ -5,10 +5,10 @@ import micMute from "../assets/mic-mute-fill.svg?url";
 
 export function ChatBar() {
     const fetcher = useFetcher();
-    const [text, setText] = useState<string | undefined>(undefined);
+    const [text, setText] = useState<string>("");
     const [isMicOpen, setIsMicOpen] = useState(false);
     return <div className="flex justify-center items-end w-full h-fit p-4">
-        <fetcher.Form className="flex w-full justify-center" method="post" autoComplete="off" onSubmit={() => setText(undefined)}>
+        <fetcher.Form className="flex w-full justify-center" method="post" autoComplete="off" onSubmit={() => setText("")}>
             <div className="flex flex-row bg-slate-600 text-slate-100 rounded-3xl px-4 py-3 w-1/2 text-xl">
                 <input
                     placeholder="Enter your Message"
