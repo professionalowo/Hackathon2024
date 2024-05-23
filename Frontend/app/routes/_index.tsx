95361ac10f6f70dc18fe432ef5a00a540670034c
enrico hu <enricomc11@gmail.com>
2024-05-23 21:56:31 +0200

    added button animation

--- Frontend/app/routes/_index.tsx
+++ Frontend/app/routes/_index.tsx
@@ -10,14 +10,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <div className="gap-5 px-5 py-3 flex flex-col w-full h-full justify-center align-middle items-center font-mono">
-      <h1 className="text-4xl">Welcome to Remix!</h1>
-      <p className="text-center text-lg">
-        This is a new Remix app. You can start editing this file immediately to
-        see how fast the hot module reloading is!
-      </p>
-      <NavLink className="px-5 py-3 bg-purple-800 hover:bg-purple-900 rounded-md" to={"/chat"}>Start Chatting</NavLink>
-    </div>
-  );
+    return (
+        <div className="gap-5 px-5 py-3 flex flex-col w-full h-full justify-center align-middle items-center font-mono">
+            <h1 className="text-4xl">Welcome to Remix!</h1>
+            <p className="text-center text-lg">
+                This is a new Remix app. You can start editing this file immediately to
+                see how fast the hot module reloading is!
+            </p>
+            <div className="nav-link-container">
+                <NavLink className="px-5 py-3 bg-purple-800 hover:bg-purple-900 rounded-md" to={"/chat"}>Start Chatting</NavLink>
+            </div>
+        </div>
+    );
 }
+
