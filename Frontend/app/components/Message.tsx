894b161fa6394b5f8d0315cefbf714ce46376ea6
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-23 20:58:03 +0200

    added cn lib function

--- Frontend/app/components/Message.tsx
+++ Frontend/app/components/Message.tsx
@@ -1,12 +1,13 @@
-import {Message} from "~/components/NavBar";
-import {useState} from "react";
+import { type Message } from "~/components/NavBar";
+import { useState } from "react";
+import { cn } from "~/lib/cn";
 
-export function Message({message: {message, ai, timestamp}}: { message: Message }) {
+export function Message({ message: { message, ai, timestamp } }: { message: Message }) {
     const [date] = useState(new Date(timestamp));
-    return <div className={"w-full flex flex-col " + (ai? "items-start":"items-end")}>
-        <div className="w-fit px-5 py-3 m-5 rounded-3xl bg-slate-600">
+    return <div className={cn("w-full flex flex-col", { "items-start": ai }, { "items-end": !ai })}>
+        <div className={cn("flex flex-col w-fit px-5 py-3 m-5 rounded-3xl bg-slate-600", { "bg-purple-800": ai })}>
             <p>{message}</p>
-            <small>{date.toLocaleTimeString()}</small>
+            <small className="flex flex-col items-end">{date.toLocaleTimeString()}</small>
         </div>
     </div>
 }
