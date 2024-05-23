ba5cf1dbc853fea8dde1921910c4f32c5388707d
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-24 01:08:52 +0200

    made text required

--- Frontend/app/components/ChatBar.tsx
+++ Frontend/app/components/ChatBar.tsx
@@ -1,14 +1,14 @@
 import { useFetcher } from "@remix-run/react"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import mic from "../assets/mic-fill.svg?url";
 import micMute from "../assets/mic-mute-fill.svg?url";
 
 export function ChatBar() {
     const fetcher = useFetcher();
-    const [text, setText] = useState<string>("");
+    const [text, setText] = useState<string | undefined>(undefined);
     const [isMicOpen, setIsMicOpen] = useState(false);
     return <div className="flex justify-center items-end w-full h-fit p-4">
-        <fetcher.Form className="flex w-full justify-center" method="post" autoComplete="off" onSubmit={() => setText("")}>
+        <fetcher.Form className="flex w-full justify-center" method="post" autoComplete="off" onSubmit={() => setText(undefined)}>
             <div className="flex flex-row bg-slate-600 text-slate-100 rounded-3xl px-4 py-3 w-1/2 text-xl">
                 <input
                     placeholder="Enter your Message"
@@ -16,6 +16,7 @@ export function ChatBar() {
                     className="w-full bg-slate-600 focus:outline-none"
                     value={text}
                     onChange={(e) => setText(e.target.value)}
+                    required
                 />
                 <button type="button" onClick={() => setIsMicOpen(o => !o)}>
                     <img alt={"mic"} src={isMicOpen ? micMute : mic}>
