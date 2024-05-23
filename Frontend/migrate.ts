0c966f2697c20493534c8c52acab839833a7a5ef
RavenOwO <samuel.feder.03@gmail.com>
2024-05-24 00:10:14 +0200

    removed error

--- Frontend/migrate.ts
+++ Frontend/migrate.ts
@@ -2,4 +2,5 @@ import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { db } from './app/.server/db/client';
 
 // This will run migrations on the database, skipping the ones already applied
+//@ts-expect-error
 await migrate(db, { migrationsFolder: './drizzle' });
