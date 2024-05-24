642e380840416dc1b54958dce9069789350f5fc7
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-24 11:42:46 +0200

    fixed searchParam state

--- Frontend/app/components/NavBar.tsx
+++ Frontend/app/components/NavBar.tsx
@@ -7,6 +7,7 @@ import { ChatWithMessages } from "~/.server/chats";
 
 export type NavBarProps = { chats: Array<ChatWithMessages> | Promise<Array<ChatWithMessages>> }
 export function NavBar({ chats }: NavBarProps) {
+
     return <NavBarSkeleton>
         <Suspense fallback={
             <>
@@ -16,7 +17,13 @@ export function NavBar({ chats }: NavBarProps) {
             <Await resolve={chats}>
                 {chats => chats.map(chat => (
                     <NavLink unstable_viewTransition key={chat.id}
-                        className="hover:bg-slate-700 cursor-pointer rounded py-2 pl-5 " to={`/chat/${chat.id}`}>
+                        className="hover:bg-slate-700 cursor-pointer rounded py-2 pl-5 "
+                        to={{
+                            pathname: `/chat/${chat.id}`, search: (function () {
+                                if (typeof document !== "undefined")
+                                    return location.search
+                            })()
+                        }}>
                         Chat {chat.id}
                     </NavLink>)
                 )}
@@ -30,7 +37,11 @@ function MessageSkeleton() {
 }
 
 export const NavBarSkeleton = ({ children }: { children?: ReactNode }) => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [isOpen, setIsOpen] = useState(searchParams.get("open") ?? false as boolean)
+    useEffect(() => {
+        setSearchParams(prev => ({ open: isOpen, ...prev }));
+    }, [isOpen])
     return <nav
         className={cn("bg-slate-800 rounded-r h-full flex flex-row justify-start align-top", { "w-1/6": isOpen })}>
         <div className="flex flex-col h-full w-full gap-2 pt-10">
@@ -42,14 +53,21 @@ export const NavBarSkeleton = ({ children }: { children?: ReactNode }) => {
             <div className="px-5 pb-4 w-full">
                 <div className="hover-animation flex items-center rounded-full bg-purple-800 text-white py-2 hover:bg-purple-900">
                     <button className="flex m-3 items-center w-full">
-                        <Link to={"/chat/new"} reloadDocument className="w-full h-fit flex align-middle items-center">
+                        <Link to={{
+                            pathname: "/chat/new", search: (function () {
+                                if (typeof document !== "undefined")
+                                    return location.search
+                            })()
+                        }} reloadDocument className="w-full h-fit flex align-middle items-center">
                             <img alt="add" src={addIcon} className="" style={{ minWidth: "32px" }} />
                             {isOpen && <span className="w-full text-center">New Chat</span>}
                         </Link>
                     </button>
                 </div>
             </div>
-            {isOpen && children}
+            <div className="overflow-auto flex flex-col gap-3">
+                {isOpen && children}
+            </div>
         </div>
-    </nav>
+    </nav >
 }
--- Frontend/app/routes/chat.new/route.ts
+++ Frontend/app/routes/chat.new/route.ts
@@ -1,7 +1,12 @@
-import { redirect } from "@remix-run/node";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { addChats } from "~/.server/chats";
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
     const chat = await addChats({ timestamp: Date.now() });
-    return redirect(`/chat/${chat[0].id}`);
+    let url = `/chat/${chat[0].id}`;
+    const requestUrl = new URL(request.url);
+    if (requestUrl.searchParams.has("open")) {
+        url += `?open=${requestUrl.searchParams.get("open")!}`
+    }
+    return redirect(url);
 }
