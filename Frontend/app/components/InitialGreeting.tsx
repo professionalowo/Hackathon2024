19b3e14ee704185fbe8d753bce2eaa2ee83f9447
Martin Brandl <martin.brandl1307@gmail.com>
2024-05-25 12:04:58 +0200

    fixed cards

--- Frontend/app/components/InitialGreeting.tsx
+++ Frontend/app/components/InitialGreeting.tsx
@@ -5,7 +5,7 @@ export function InitialGreeting(props: React.DetailedHTMLProps<React.HTMLAttribu
     return <div {...props}>
         <img src={twoPeeps} alt="twoPeeps" className="w-1/6 mx-auto mt-10" />
         <h1 className="text-4xl text-center font-light">Hey! How can I assist you today?</h1>
-        <div className='h-full w-full px-10 gap-10 flex flex-row justify-between align-middle pt-10'>
+        <div className='h-full w-full px-10 gap-10 flex flex-row justify-between align-middle pt-5'>
             <Card to={"/chat"}>
                 <h1 className='text-xl'>Errors</h1>
                 <p>Ask me anytime you encounter an error!</p>
@@ -22,7 +22,7 @@ export function InitialGreeting(props: React.DetailedHTMLProps<React.HTMLAttribu
     </div>
 }
 function Card({ children, to }: { children: ReactNode, to: LinkProps["to"] }) {
-    return <Link to={to} className='flex flex-col justify-center items-center text-center text-2xl gap-3 h-2/5 w-1/3 px-4 py-2 rounded shadow-sm shadow-slate-500  card-gradient hover-animation-s overflow-clip'>
+    return <Link to={to} className='flex flex-col justify-center items-center text-center text-2xl gap-3 m-5 w-1/3 px-4 py-2 rounded shadow-sm shadow-slate-500  card-gradient hover-animation-s overflow-clip'>
         {children}
     </Link>
 }
